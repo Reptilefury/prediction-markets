d3e20a41db9279179d37fa892268797c3b0157c6
Reptilefury <ryanwa@ueab.ac.ke>
2025-12-05 15:38:42 +0300

    fix: Remove unnecessary cacheService stubbing from test
    - Remove unused cacheService.set() mock that was causing UnnecessaryStubbingException
    - Test execution path doesn't actually use this mock

--- src/test/java/com/oregonMarkets/domain/user/service/UserRegistrationServiceTest.java
+++ src/test/java/com/oregonMarkets/domain/user/service/UserRegistrationServiceTest.java
@@ -70,7 +70,6 @@ class UserRegistrationServiceTest {
         when(proxyWalletOnboardingService.createUserProxyWallet(anyString())).thenReturn(Mono.just("0x456"));
         when(enclaveClient.createUDA(anyString(), anyString(), anyString(), anyString())).thenReturn(Mono.just(udaResponse));
         when(userRepository.save(any(User.class))).thenReturn(Mono.just(savedUser));
-        when(cacheService.set(anyString(), any(), any())).thenReturn(Mono.empty());
 
         // Then
         StepVerifier.create(userRegistrationService.registerUser(request, magicUser, didToken))
